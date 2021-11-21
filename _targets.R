8c54c23e1532a13642d56f475e6498b88f08d452
Patrick Anker <patricksanker@gmail.com>
2021-11-21 13:29:20 -0500

    add motivation section to 01

--- _targets.R
+++ _targets.R
@@ -15,16 +15,40 @@ rlang::list2(
         base_color = "#1f0536",
         header_font_google = google_font("Cookie"),
         text_font_google   = google_font("Montserrat", "300 ", "300i"),
-        code_font_google   = google_font("Fira Mono")
+        code_font_google   = google_font("Fira Mono"),
+        colors = c(
+          red = "#c50f3c",
+          yellow = "#ecaa00"
+        )
       ),
       style_extra_css(
         css = list(
           ":root" = list(
             "--header-h1-font-size" = "3rem",
-            "--header-h2-font-size" = "2rem"
+            "--header-h2-font-size" = "2rem",
+            "--font12" = "12pt",
+            "--font10" = "10pt"
           ),
           "h2, h3" = list(
             "font-family" = "var(--text-font-family)"
+          ),
+          ".font12 > table" = list(
+            "font-size" = "var(--font12)"
+          ),
+          ".font12 > p" = list(
+            "font-size" = "var(--font12)"
+          ),
+          ".font12 > pre .remark-code-line" = list(
+            "font-size" = "var(--font12)"
+          ),
+          ".font10 > table" = list(
+            "font-size" = "var(--font10)"
+          ),
+          ".font10 > p" = list(
+            "font-size" = "var(--font10)"
+          ),
+          ".font10 > pre .remark-code-line" = list(
+            "font-size" = "var(--font10)"
           ),
           ".ties-footer" = list(
             position = "fixed",
@@ -33,6 +57,11 @@ rlang::list2(
           ),
           ".ties-footer p img" = list(
             width = "30%"
+          ),
+          ".definition" = list(
+            "background-color" = "#bde9fd",
+            padding = "5px 15px",
+            border = "1px solid"
           )
         )
       )
